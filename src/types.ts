fb4094aac54aca6de0c72ae0f39d375a37e9cbbc
samlite67 <Yusufsamodin67@gmail.com>
2026-01-12 07:29:47 +0000

    Remove wrangler.jsonc configuration file as it is no longer needed for the project setup.

--- src/types.ts
+++
@@ -1,8 +0,0 @@
-import type { Context } from "hono";
-
-export type AppContext = Context<{ Bindings: Env }>;
-export type HandleArgs = [AppContext];
-
-export interface SimulationState {
-	[key: string]: any;
-}
